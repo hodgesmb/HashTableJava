0f9ca8cfd208ba0f61163258e42cf12507357a55
Montana Hodges <mbh022@shsu.edu>
2015-01-03 01:16:27 -0600

    Fixing main to use Table and Item objects

--- HashTable/src/hashtable/HashTable.java
+++ HashTable/src/hashtable/HashTable.java
@@ -16,16 +16,48 @@ public class HashTable {
 
     /**
      * @param args the command line arguments
+     * 
+     * 
+     * 
      */
+    
+        private static void clearScreen(){
+            
+        final String ANSI_CLS = "\u001b[2J";
+        final String ANSI_HOME = "\u001b[H";
+        System.out.print(ANSI_CLS + ANSI_HOME);
+        System.out.flush();
+            
+    }
+                
+    public static void printMenu(){
+        
+        clearScreen();
+        System.out.println("Enter the number for what you wish to do:");
+        System.out.println("1.) Add an item to table.");
+        System.out.println("2.) Remove an item from table.");
+        System.out.println("3.) Print table.");
+        System.out.println("4.) Print table count.");
+        System.out.println("5.) Empty table.");
+        System.out.println("6.) Exit program.");
+        
+    }
+    
+    private static void exitProg(){
+        
+        System.exit(0);
+        
+    }
+    
     public static void main(String[] args) throws NullPointerException{
         
+        Table hashTable;
+        Item currentItem;
         String tableName;
-        String item;
-        String hashVal;
-        String[] hashTable;
+        String itemName;
+        int itemID;
         int numItems;
         int menuSelect;
-        int tableIndex = 0;
 
         Scanner kbd = new Scanner(System.in);
         System.out.println("Hash Table Program");
@@ -45,8 +77,8 @@ public class HashTable {
             
         }
         
-        System.out.println("You've chosen the table size to be " + numItems + ". \n A table named " + tableName+ " with " + numItems + " item slots will be created.");
-        hashTable = new String[numItems];
+        System.out.println("You've chosen the table size to be " + itemCount + ". \n A table named " + tableName+ " with " + numItems + " item slots will be created.");
+        hashTable = new Table(tableName, numItems);
         
         try{
         
@@ -57,7 +89,9 @@ public class HashTable {
                 
                 if(menuSelect < 1 || menuSelect > 5){
                     
+                    
                     System.out.println("Please enter a number between 1 and 5 inclusive.");
+                    printMenu();
                     menuSelect = kbd.nextInt();
                     
                 }
@@ -66,12 +100,20 @@ public class HashTable {
                     
                     case 1:
                         
-                        addItems();
+                        System.out.println("Enter the name of an item: ");
+                        itemName = kbd.next();
+                        System.out.println("Enter an item ID number: ");
+                        itemID = kbd.nextInt();
+                        currentItem = new Item(itemName, itemID);
+                        hashTable.addItem(currentItem);
                         break;
                         
                     case 2:
                         
-                        deleteItem();
+                        System.out.println("Enter the name of an item to be deleted: ");
+                        itemName = kbd.next();
+                        currentItem = new Item(itemName, 0);
+                        deleteItem(currentItem);
                         break;
                         
                     case 3:
@@ -114,57 +156,6 @@ public class HashTable {
         
     }
         
-    private static void clearScreen(){
-            
-        final String ANSI_CLS = "\u001b[2J";
-        final String ANSI_HOME = "\u001b[H";
-        System.out.print(ANSI_CLS + ANSI_HOME);
-        System.out.flush();
-            
-    }
-                
-    public static void printMenu(){
-        
-        clearScreen();
-        System.out.println("Enter the number for what you wish to do:");
-        System.out.println("1.) Add items to table.");
-        System.out.println("2.) Remove an item from table.");
-        System.out.println("3.) Print table.");
-        System.out.println("4.) Print table count.");
-        System.out.println("5.) Empty table.");
-        System.out.println("6.) Exit program.");
-        
-    }
-    
-    public static int addItems(){
-        
-        String itemHolder;
-        int hashHolder;
-        clearScreen();
-        System.out.println("There are " + printTableCount() + " remaining.  Please enter an item name:");
-        itemHolder = kbd.next();
-        hashHolder = hashItem(itemHolder);
-        
-        
-        
-        
-        
-        
-        
-        
-    }
-    
-    private static int hashItem(String inputStr){
-        
-        return inputStr.length();
 
-    }
-      
-    private static void exitProg(){
-        
-        System.exit(0);
-        
-    }
-    
     
 }
