1068b43c4e56e119b8d06e302e301f657e2068d5
Montana Hodges <mbh022@shsu.edu>
2015-01-02 21:28:57 -0600

    Change 1:  Added Hello World! and System.exit

--- HashTable/src/hashtable/HashTable.java
+++ HashTable/src/hashtable/HashTable.java
@@ -15,7 +15,10 @@ public class HashTable {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Hello World!");
+        System.exit(0); 
+        
     }
     
 }
